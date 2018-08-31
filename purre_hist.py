6ee26b06dc0aae8f397508ef733951b583fadcdc
Gill Erick <ogayogill9@gmail.com>
2018-08-31 08:41:24 -0400

    Added pure histograms in python

---
+++ purre_hist.py
@@ -0,0 +1,14 @@
+a = (3, 5, 6, 87, 78, 78, 9, 7, 5)
+
+
+def count_elements(sequence) -> dict:
+    # Tally elements from sequence
+    hist = {}
+    for i in sequence:
+        hist[i] = hist.get(i, 0) + 1
+    return hist
+
+
+counted = count_elements(a)
+print(counted)
+
